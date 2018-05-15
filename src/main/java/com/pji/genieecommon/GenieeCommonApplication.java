ca9d6cc8d129b28477be8dbd35540e074d955221
Jeffry <jeffry.christian@pji-solutions.com>
2018-05-15 08:45:08 +0700

    first commit

---
+++ src/main/java/com/pji/genieecommon/GenieeCommonApplication.java
@@ -0,0 +1,12 @@
+package com.pji.genieecommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenieeCommonApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(GenieeCommonApplication.class, args);
+    }
+}
